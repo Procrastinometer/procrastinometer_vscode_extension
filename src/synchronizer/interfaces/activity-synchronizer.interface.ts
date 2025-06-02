d02b759427d812f640f1c7f249eaf62a69096f65
Bogdan Yarmolka <65012653+thebladehit@users.noreply.github.com>
2025-06-02 23:44:27 +0300

    Add syncing server time with local time (#6)

--- src/synchronizer/interfaces/activity-synchronizer.interface.ts
+++ src/synchronizer/interfaces/activity-synchronizer.interface.ts
@@ -1,5 +1,5 @@
 export interface ActivitySynchronizer {
-  init(syncInterval: number): void;
+  init(syncInterval: number, callback: Function): void;
   stop(): void;
   syncActivity(): Promise<void>;
 }
